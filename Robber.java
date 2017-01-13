e936bd4c14a059d9f9f185fa863064107057b1c7
Anna Grosul <ag235@st-andrews.ac.uk>
2017-01-13 16:50:32 -0500

    constructor added to the Robber class

--- Robber.java
+++ Robber.java
@@ -3,6 +3,18 @@
 
 public class Robber{
 
+	//robber will be put on different coordinates based on where the player has placed it 
+	private Coordinate  c = new Coordinate(); 
+	// p will represent the current player that moved the robber
+	//if the player is null then no one is responsible for moving the robber at the moment 
+	private Player  p = new Player(); 
+
+
+	public Robber(Coordinate coordinate , Player player){ 
+
+	}
+
+
 
 
 
